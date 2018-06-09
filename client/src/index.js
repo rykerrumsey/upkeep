cfda41906e2c0683d4170df700457405c0757d59
Ryker Rumsey <rykerrumsey@outlook.com>
2018-06-09 15:16:04 -0600

    client: add font-awesome to build, implement shuffle.js ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,12 @@
 import './scss/main.scss'
 import 'bulma/bulma.sass'
+
+import Shuffle from 'shufflejs'
+
+var element = document.querySelector('.grid');
+var sizer = element.querySelector('.sizer-element');
+
+var shuffleInstance = new Shuffle(element, {
+  itemSelector: '.vehicle-box',
+  sizer: sizer // could also be a selector: '.my-sizer-element'
+});
